53410fecb95ba4da966f53a4d13354e2605d0164
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2023-03-22 15:39:15 -0600

    sh1Sum func

--- sha1/sha1.go
+++ sha1/sha1.go
@@ -1,11 +1,44 @@
 package main
 
-import "os"
+import (
+	"compress/gzip"
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	sig, err := sha1Sum("http.log.gz")
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+	fmt.Println(sig)
+}
 
 // $ cat http.log.gz| gunzip | sha1sum
 func sha1Sum(fileName string) (string, error){
-	file, err := os.Open(filename)
+	// idiom: acquire a resource, check for error, defer release
+	file, err := os.Open(fileName)
 	if err != nil {
 		return "", nil
 	}
+	defer file.Close()
+
+	r, err := gzip.NewReader(file)
+	if err != nil {
+		return "", err
+	}
+	defer r.Close()
+
+	// io.CopyN(os.Stdout, r, 100)
+	w := sha1.New()
+
+	if _, err := io.Copy(w, r); err != nil {
+		return "", err
+	}
+
+	sig := w.Sum(nil)
+	return fmt.Sprintf("%x", sig), nil
 }
